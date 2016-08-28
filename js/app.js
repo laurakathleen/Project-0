6512bfe133ed08b538a08088601a9cc23525ff4e
laurakathleen <laurakwalley@gmail.com>
2016-08-28 16:06:40 -0700

    gets code condensed and DRY

--- js/app.js
+++ js/app.js
@@ -1,63 +1,95 @@
-//Testing to see if push is working
-
-var x = $('#x');
-var o =  $('#o');
-var $plane = $('.plane-icon');
-//constructor for all types of tokens- Obstacles & Boosts:
-function Token(option) {
-		this.symbol = option.symbol;
-	}
-//instances created from Token:
-var obstacle = new Token({symbol: '<p>X</p>'});
-var boost = new Token({symbol: '<p>O</p>'});
+var plane = document.getElementsByClassName('plane-icon');
+var start = document.getElementsByClassName('start-btn');
+var obstacle = document.getElementsByClassName('obstacle-icon');
+var boost = document.getElementsByClassName('boost-icon');
+var gameBoard = document.getElementsByClassName('game-board');
+var tokePosition;
 
 $(document).ready(function(){
 	//test to make sure file is working:
 	console.log("Sanity check!");
-	
-	//set interval so that every second, an obstacle is dropped:
-	//window.setInterval(1000);
-	
-	//function to literally drop the obstacle: (set to start on btn click for now)
-	//$('.drop-obstacle-btn').on('click', dropObstacle(){
 
-	//})
-
-	//define dropObstacle function
-	function dropObstacle(){
-		x.slideDown('slow');
-		window.setInterval(1000);
-	}
-	dropObstacle();
-
+	/**********PLANE MOVEMENT***********/
 	$("body").keydown(function(e){
 		if (e.which === 39) {
 			//move right
-			console.log('right');
-			$('.plane-icon').each(function() {
+			$(plane).each(function() {
 				//get position of the plane
-				var planeRPosition = $('.plane-icon').position();
-				var planeRight = $('.plane-icon').position().left;
+				var planeRPos = $(plane).position();
+				var planeRight = $(plane).position().left;
 				//if the position of the plane is less than or equal to 220px on the right (parent div width), keep moving; else stop movement
 				if (planeRight <= 220) {
 	   		 		$(this).css({ left: $(this).position().left + 10 });
-	   		 		planeRPosition + 10;
+	   		 		planeRPos + 10;
    		 		}
 			});
 		} else if (e.which === 37) {
 			//move left
-			console.log('left');
-			$('.plane-icon').each(function() {
+			$(plane).each(function() {
 				//get position of plane
-				var planeLPosition = $('.plane-icon').position();
-				var planeLeft = $('.plane-icon').position().left;
+				var planeLPos = $(plane).position();
+				var planeLeft = $(plane).position().left;
 				//if the position of the plane is greater than or equal to -240px on the left (parent div width), keep moving; else stop movement
 				if (planeLeft >= -240) {
     			$(this).css({ left: $(this).position().left - 10 });
-    				planeLPosition - 10;
+    				planeLPos - 10;
 	   		 	}
 			});
 		}
 	})
+
+	/***********END OF PLANE MOVEMENT**************/
+
+	//rollTheDice to see if an obstacle or a boost will drop:
+	function rollTheDice(){
+		tokenPosition = Math.floor(Math.random() * 20);
+		console.log("when i rolled the dice, i got: " + tokenPosition);
+		if (tokenPosition % 2 == 0){
+			getTokenStartPos(obstacle);
+		} else {
+			getTokenStartPos(boost);
+		}
+	}
+	
+	//getTokenStartPos to figure where to drop the obstacle/boost:
+	function getTokenStartPos(token){
+		var tokenPos = Math.floor(Math.random() * 20);
+		console.log("When getTokenStart ran, I got: " + tokenPos);
+		if (tokenPos <= 5){
+			$(token).css('margin-left', '90px');
+		} else if (tokenPos > 5 && tokenPos <= 10){
+			$(token).css('margin-left', '180px');
+		} else if (tokenPos > 10 && tokenPos <= 15){
+			$(token).css('margin-left', '270px');
+		} else if (tokenPos > 15 && tokenPos <= 20){
+			$(token).css('margin-left', '360px');
+		} else {
+			$(token).css('margin-left', '440px');
+		}
+		console.log(token);
+		dropIt(token);
+	}
+
+	function dropIt(token){
+		window.setInterval(200);
+		var positionOfToken = $(token).position().top;
+		console.log("When I got to dropIt: " + positionOfToken);
+		if (positionOfToken > 0 && positionOfToken < 260){
+			$(token).css({ top: $(token).position().top + 10}, 200);
+		} else if (positionOfToken >= 260){
+			rollTheDice();
+		}
+	}
+
+	// function dropping(token){
+	// 	$(token).css({ top: $(token).position().top + 10}, 200);
+	// 	console.log("Dropping!");
+	// }
+
+	//clicking the start button triggers rollTheDice:
+	$(start).on('click', function startIt(){
+		rollTheDice();
+	})
+
 
 })
